2e9d5efebe862af2051dbf43b1958be9e2582a22
Martin1553 <martinsebastian1553@outlook.com>
2024-01-09 13:36:04 -0300

    api key añadido

--- index.js
+++ index.js
@@ -5,32 +5,32 @@ const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 const app = express();
 
 const getBrowser = () =>
-  IS_PRODUCTION
-    ? // Connect to browserless so we don't run Chrome on the same hardware in production
-      puppeteer.connect({ browserWSEndpoint: 'wss://chrome.browserless.io?token=YOUR-API-TOKEN' })
-    : // Run the browser locally while in development
-      puppeteer.launch();
+    IS_PRODUCTION
+        ? // Connect to browserless so we don't run Chrome on the same hardware in production
+        puppeteer.connect({ browserWSEndpoint: 'wss://chrome.browserless.io?token=a56eb8de-f795-404e-851d-f8139c26eef2' })
+        : // Run the browser locally while in development
+        puppeteer.launch();
 
 app.get('/image', async (req, res) => {
-  let browser = null;
+    let browser = null;
 
-  try {
-    browser = await getBrowser();
-    const page = await browser.newPage();
+    try {
+        browser = await getBrowser();
+        const page = await browser.newPage();
 
-    await page.goto('http://www.example.com/');
-    const screenshot = await page.screenshot();
+        await page.goto('http://www.example.com/');
+        const screenshot = await page.screenshot();
 
-    res.end(screenshot, 'binary');
-  } catch (error) {
-    if (!res.headersSent) {
-      res.status(400).send(error.message);
+        res.end(screenshot, 'binary');
+    } catch (error) {
+        if (!res.headersSent) {
+            res.status(400).send(error.message);
+        }
+    } finally {
+        if (browser) {
+            browser.close();
+        }
     }
-  } finally {
-    if (browser) {
-      browser.close();
-    }
-  }
 });
 
 app.listen(8080, () => console.log('Listening on PORT: 8080'));
